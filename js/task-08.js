28ce58cfdb633ea7ce422f81c9287a5486ace48e
artemkravchenk0 <kravchenkoartem@ukr.net>
2023-02-24 23:42:54 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', (event) =>{
+    event.preventDefault();
+    const emailEl = document.querySelector('[type=email]');
+    const passwordEl = document.querySelector('[type=password]');   
+    
+    if(emailEl.value === '' ||  passwordEl.value === '') {
+        alert("Заповныть будь ласка всі поля");
+        return
+    }
+    const obj = {
+        email: emailEl.value,
+        password: passwordEl.value
+    };
+    console.log(obj);
+    formEl.reset()
+})
