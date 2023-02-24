2c17246d7714a66e49a53055dcfd35f9622b44cd
artemkravchenk0 <kravchenkoartem@ukr.net>
2023-02-24 20:31:27 +0200

    task 2 comp

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulEl = document.querySelector('#ingredients')
+
+const liIngridients = ingredients.map(element => {
+  const liEl = document.createElement('li');
+  liEl.classList.add('item');
+  liEl.textContent = element;
+  return liEl;
+});
+
+ulEl.append(...liIngridients);
